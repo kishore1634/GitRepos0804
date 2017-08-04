f91c7a446a0d80464213a653e619fcd38c6bedcd
Kishore <kishore1634@gmail.com>
2017-08-04 11:48:43 +0800

    Zip code added.

--- GitPractice0804/src/com/forrest/Git0804.java
+++ GitPractice0804/src/com/forrest/Git0804.java
@@ -8,10 +8,12 @@ public class Git0804 {
 			String myEmail ="kishore1634@yahoo.com";
 			String myAddress = "Beijing,China.";
 			int myPhoneNumber = 18600;
+			int zipCode = 1234;
 			System.out.println(myName);
 			System.out.println(myEmail);
 			System.out.println(myAddress);
-			System.out.println(myPhoneNumber);
+			System.out.println( myPhoneNumber);
+			System.out.println("ZipCode:" + zipCode);
 			System.out.println("========================");
 			 
 			 for(int i=0; i<5; i++) {
