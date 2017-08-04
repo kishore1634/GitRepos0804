f04803b863f9092f261d586d1d9529ad99383d3d
Kishore <kishore1634@gmail.com>
2017-08-04 11:27:21 +0800

    This is the first commit.

---
+++ GitPractice0804/src/com/forrest/Git0804.java
@@ -0,0 +1,21 @@
+package com.forrest;
+
+public class Git0804 {
+
+	public static void main(String[] args) {
+		
+		 String myName = "Kishore Biswas";
+			String myEmail ="kishore1634@yahoo.com";
+			String myAddress = "Beijing,China.";
+			//int myPhoneNumber = 18600;
+			System.out.println(myName);
+			System.out.println(myEmail);
+			System.out.println(myAddress);
+			//System.out.println(myPhoneNumber);
+			//System.out.println("========================");
+			
+			//System.out.println("First change after first push.");
+
+	}
+
+}
