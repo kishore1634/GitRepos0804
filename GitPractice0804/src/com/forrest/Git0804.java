dd6ee9693c41f445e1cf1e358ed1293d5c241bb4
Kishore <kishore1634@gmail.com>
2017-08-04 11:33:37 +0800

    The first change after pushing.

--- GitPractice0804/src/com/forrest/Git0804.java
+++ GitPractice0804/src/com/forrest/Git0804.java
@@ -7,11 +7,11 @@ public class Git0804 {
 		 String myName = "Kishore Biswas";
 			String myEmail ="kishore1634@yahoo.com";
 			String myAddress = "Beijing,China.";
-			//int myPhoneNumber = 18600;
+			int myPhoneNumber = 18600;
 			System.out.println(myName);
 			System.out.println(myEmail);
 			System.out.println(myAddress);
-			//System.out.println(myPhoneNumber);
+			System.out.println(myPhoneNumber);
 			//System.out.println("========================");
 			
 			//System.out.println("First change after first push.");
